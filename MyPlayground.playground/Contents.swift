c90b3f760836aef29b07d54ca81ec1afa487bce7
erJohn84 <john.veronelli@gmail.com>
2016-05-30 14:04:43 -0430

    Prueba de semana 2

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,4 +2,17 @@
 
 import UIKit
 
-var str = "Hello, playground"
+for index in 0...100{
+    print(index)
+    if index % 5 == 0{
+        print("# \(String(index)) Bingo")
+    }
+    if index % 2 == 0{
+        print("# \(String(index)) Par")
+    }else{
+        print("# \(String(index)) Impar")
+    }
+    if index >= 30 && index <= 40{
+        print("# \(String(index)) Viva Swift!")
+    }
+}
